b56af2fb4ddfa4d36ad65124cc27b4c41344ee78
Jojo Lindell <johannes152@hotmail.com>
2025-02-19 14:59:22 +0100

    Created WebSocket

--- MauiBackend/Program.cs
+++ MauiBackend/Program.cs
@@ -1,5 +1,6 @@
 using MauiBackend.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
 
@@ -33,13 +34,48 @@ namespace MauiBackend
 
             var app = builder.Build();
 
-            app.UseHttpsRedirection();
-            
+            app.UseWebSockets();
+
             app.UseAuthentication();
             app.UseAuthorization();
 
+            app.Map("/ws", websocketApp =>
+            {
+                websocketApp.Run(async context =>
+                {
+                    if (context.WebSockets.IsWebSocketRequest)
+                    {
+                        var webSocket = await context.WebSockets.AcceptWebSocketAsync();
+                        await HandleWebSocketAsync(webSocket);
+                    }
+                    else
+                    {
+                        context.Response.StatusCode = 400;
+                    }
+                });
+            });
+
             app.MapControllers();
             app.Run();
+        }
+
+        private static async Task HandleWebSocketAsync(System.Net.WebSockets.WebSocket webSocket)
+        {
+            var buffer = new byte[1024 * 4];
+            while (webSocket.State == System.Net.WebSockets.WebSocketState.Open)
+            {
+                var result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+                if (result.MessageType == System.Net.WebSockets.WebSocketMessageType.Text)
+                {
+                    var message = System.Text.Encoding.UTF8.GetString(buffer, 0, result.Count);
+                    Console.WriteLine($"Received message: {message}");
+
+                    await webSocket.SendAsync(new ArraySegment<byte>(buffer, 0, result.Count),
+                        System.Net.WebSockets.WebSocketMessageType.Text,
+                        true,
+                        CancellationToken.None);
+                }
+            }
         }
     }
 }
